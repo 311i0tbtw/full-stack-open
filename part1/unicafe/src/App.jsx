637f457f4ed834dd4c0f3dd0871ccc3b2a88a3ce
311i0tbtw <saishinnthant19@gmail.com>
2026-06-03 14:30:01 +0700

    finished exe 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,19 +4,20 @@ const Button = ({ onClick, text }) => {
     return <button onClick={onClick}>{text}</button>;
 };
 
-const Total = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad }) => {
     const all = good + neutral + bad;
     const average = all === 0 ? 0 : (good - bad) / all;
+    // positive should be good / all * 100
     const positive = all === 0 ? 0 : (good / all) * 100;
 
     return (
         <div>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
-            <p>average {average}</p>
-            <p>positive {positive} %</p>
+            <p>good: {good}</p>
+            <p>neutral: {neutral}</p>
+            <p>bad: {bad}</p>
+            <p>all: {all}</p>
+            <p>average: {average.toFixed(2)}</p>
+            <p>positive: {positive.toFixed(1)}%</p>
         </div>
     );
 };
@@ -34,9 +35,52 @@ const App = () => {
             <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
             <Button onClick={() => setBad(bad + 1)} text="bad" />
             <h2>statistics</h2>
-            <Total good={good} neutral={neutral} bad={bad} />
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     );
 };
 
 export default App;
+
+// import { useState } from "react";
+
+// const Button = ({ onClick, text }) => {
+//     return <button onClick={onClick}>{text}</button>;
+// };
+
+// const Statistics = ({ good, neutral, bad }) => {
+//     const all = good + neutral + bad;
+//     const average = all === 0 ? 0 : (good - bad) / all;
+//     const positive = all === 0 ? 0 : (good / all) * 100;
+
+//     return (
+//         <div>
+//             <p>good {good}</p>
+//             <p>neutral {neutral}</p>
+//             <p>bad {bad}</p>
+//             <p>all {all}</p>
+//             <p>average {average}</p>
+//             <p>positive {positive} %</p>
+//         </div>
+//     );
+// };
+
+// const App = () => {
+//     // save clicks of each button to its own state
+//     const [good, setGood] = useState(0);
+//     const [neutral, setNeutral] = useState(0);
+//     const [bad, setBad] = useState(0);
+
+//     return (
+//         <div>
+//             <h2>give feedback</h2>
+//             <Button onClick={() => setGood(good + 1)} text="good" />
+//             <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+//             <Button onClick={() => setBad(bad + 1)} text="bad" />
+//             <h2>statistics</h2>
+//             <Statistics good={good} neutral={neutral} bad={bad} />
+//         </div>
+//     );
+// };
+
+// export default App;
